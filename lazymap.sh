25f6f192e293e8f63260118f490770d75bebcf24
trevorbryant <trevorbryant@gmail.com>
2020-03-01 19:19:21 -0500

    refactored

--- lazymap.sh
+++ lazymap.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-if [ -f /usr/bin/nmap ]
-then
-	/usr/bin/nmap -p- -oA $1 $2 2>/dev/null
-	ports=$(grep / $1.nmap | cut -d/ -f1 | sed -n '1!p' | xargs | sed 's/ /,/g')
-	/usr/bin/nmap -sC -sV -p$ports -oA $1 $2	
-else
-	echo "Cannot find nmap. Is the package installed?"
+# credits to calebstewart
+
+if [ $(which nmap) ] && [ -f $(which masscan) ]; then
+	info "enumerating all tcp ports w/ masscan"
+	sudo masscan -p 1-65535 $1 -p 0-65535 --max-rate 1000 -oG masscan-tcp.grep -e "$2"
+
+	# grab the list of open ports
+	ports=`grep "open" masscan-tcp.grep | awk '{ print $5 }' | cut -d'/' -f1 | tr '\n' ',' | sed 's/,$//g'`
+	
+	if [ -z "$ports" ]; then
+		warning "no open tcp ports detected!"
+	else
+		# Start nmap scan for these ports
+		info "scanning open tcp ports w/ nmap ($ports)"
+		nmap -sC -sV -sV $1 -p "$ports" -oN open-tcp.nmap
+	fi
 fi
+
